f0427ffae29775414f4f76cc16c557af52bc8fbb
Donghyeon Kim <donghyeon.kim@tacocube.com>
2018-11-29 18:11:35 +0900

    Update ensure-package function
    When installing a package, also add it to package-selected-packages,
    so that it does not get removed when package-autoremove is called.

--- .emacs.d/donghyeon/ensure-package.el
+++ .emacs.d/donghyeon/ensure-package.el
@@ -3,6 +3,10 @@
   "Ensure that PACKAGE is installed."
   (unless (package-installed-p package)
     (package-refresh-contents)
-    (package-install package)))
+    (package-install package)
+    (add-to-list 'package-selected-packages package))
+  ;; Add to the selected packages list.
+  (add-to-list 'package-selected-packages package))
+
 
 (provide 'ensure-package)

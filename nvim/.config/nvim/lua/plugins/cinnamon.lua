a579b295013242d75e75bb96f1bb3087d47da343
Danny Kim <0916dhkim@gmail.com>
2025-01-03 01:35:17 -0500

    Update nvim config

--- nvim/.config/nvim/lua/plugins/cinnamon.lua
+++ nvim/.config/nvim/lua/plugins/cinnamon.lua
@@ -2,11 +2,6 @@ return {
   "declancm/cinnamon.nvim",
   config = function()
     require("cinnamon").setup({
-      -- Enable all provided keymaps
-      keymaps = {
-        basic = true,
-        extra = true,
-      },
       options = {
         max_delta = {
           time = 80,
